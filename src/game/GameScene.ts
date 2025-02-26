be2ce38b89d158d6697a85abd1024bfbaffef9f5
Andrew Ahn <andrewahn21@gmail.com>
2025-02-26 00:45:56 -0800

    forest generation + out of bounds bug fix

--- src/game/GameScene.ts
+++ src/game/GameScene.ts
@@ -1,125 +1,173 @@
 import Phaser from "phaser";
 
 export default class GameScene extends Phaser.Scene {
-  private player!: Phaser.Physics.Arcade.Sprite;
-  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
-  private bgWidth = 2370; // Width of your background image (for some reason, it's half the actual width (actual: 4000))
-  private bgHeight = 2370; // Height of your background image (for some reason, it's half the actual height (actual: 4000))
-  private xCoord!: Phaser.GameObjects.Text;
-  private yCoord!: Phaser.GameObjects.Text;
-  private bg!: Phaser.GameObjects.Image;
-  // the background is divided into a n x n grid full of cells
-  private dimension = 40;
-  private cellWidth = 0;
-  private cellHeight = 0; 
-  // number of moves made in the horizontal and vertical directions (right and bottom are +ve)
-  private numHor = 0;
-  private numVer = 0;
-  private moveEvent: Phaser.Time.TimerEvent | null = null;
-  // initial player coordinates
-  private initPlayerX = 0;
-  private initPlayerY = 0;
+    private player!: Phaser.Physics.Arcade.Sprite;
+    private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+    private bgWidth = 2370; // Width of your background image (for some reason, it's half the actual width (actual: 4000))
+    private bgHeight = 2370; // Height of your background image (for some reason, it's half the actual height (actual: 4000))
+    private xCoord!: Phaser.GameObjects.Text;
+    private yCoord!: Phaser.GameObjects.Text;
+    private bg!: Phaser.GameObjects.Image;
+    // the background is divided into a n x n grid full of cells
+    private dimension = 80;
+    private cellWidth = 0;
+    private cellHeight = 0; 
+    // number of moves made in the horizontal and vertical directions (right and bottom are +ve)
+    private numHor = 0;
+    private numVer = 0;
+    private moveEvent: Phaser.Time.TimerEvent | null = null;
+    // "center" coordinates (because (0,0) isn't really the "center" of this scene)
+    private centerX = 0;
+    private centerY = 0;
 
-  constructor() {
-    super("GameScene");
-  }
-
-  preload() {
-    this.load.image("background", "/assets/bg.png");
-    this.load.spritesheet("player", "/assets/bron.png", { frameWidth: 60, frameHeight: 60 });
-    this.load.image("github", "/assets/github-mark.png");
-  }
-
-  create() {
-    // background and character
-    this.bg = this.add.image(window.innerWidth/2, window.innerHeight/2, "background");
-    console.log(this.bg.width, this.bg.height);
-    this.bgWidth = this.bg.width;
-    this.bgHeight = this.bg.height;
-    this.player = this.physics.add.sprite(window.innerWidth/2, window.innerHeight/2, "player");
-    this.initPlayerX = window.innerWidth/2;
-    this.initPlayerY = window.innerHeight/2;
-    // set cell dimensions
-    this.cellWidth = this.bgWidth / this.dimension;
-    this.cellHeight = this.bgHeight / this.dimension;
-
-    // coordinates
-    this.xCoord = this.add.text(20,20,'X: 0', { fontSize: '20px', fill: '#256' });
-    this.xCoord.setScrollFactor(0);
-    this.yCoord = this.add.text(20,40,'Y: 0', { fontSize: '20px', fill: '#256' });
-    this.yCoord.setScrollFactor(0);
-
-    // links
-    const githubButton = this.add.image(window.innerWidth/2, window.innerHeight/2 - 100, "github").setScale(0.25);
-    githubButton.setInteractive();
-    githubButton.on("pointerdown", () => {
-        window.open("https://github.com/andrewahn-ubc", "_blank"); // Open link in new tab
-    });
-
-    // welcome
-    this.add.text(window.innerWidth/2 - 400, window.innerHeight/2 - 200, 'Hey, welcome to my website!', { fontSize: '50px', fill: '#000' });
-
-    // player can't move "off" the background
-    this.physics.world.setBounds(
-        -this.bgWidth/2 + window.innerWidth, 
-        -this.bgHeight/2 + window.innerHeight, 
-        this.bgWidth - window.innerWidth, 
-        this.bgHeight - window.innerHeight);
-    this.player.setCollideWorldBounds(true);
-
-    // camera follows the player
-    this.cameras.main.startFollow(this.player, true, 1, 1);
-
-    this.anims.create({
-      key: "left",
-      frames: [{key: "player", frame: 0}],
-      frameRate: 20
-    });
-
-    this.anims.create({
-        key: "front",
-        frames: [{key: "player", frame: 1}],
-        frameRate: 20
-    });
-
-    this.anims.create({
-        key: "right",
-        frames: [{key: "player", frame: 2}],
-        frameRate: 20
-    });
-      
-    if (this.input.keyboard) {
-        this.cursors = this.input.keyboard.createCursorKeys();
-    }
-  }
-
-  update() {
-    this.player.setVelocity(0);
-
-    this.player.anims.play('front')
-    this.xCoord.setText("X: " + this.player.x)
-    this.yCoord.setText("Y: " + this.player.y)
-
-    if (Phaser.Input.Keyboard.JustDown(this.cursors.right)) {
-        this.moveCharacter('right')
-    } else if (Phaser.Input.Keyboard.JustDown(this.cursors.left)) {
-        this.moveCharacter('left')
-    } else if (Phaser.Input.Keyboard.JustDown(this.cursors.up)) {
-        this.moveCharacter('up')
-    } else if (Phaser.Input.Keyboard.JustDown(this.cursors.down)) {
-        this.moveCharacter('down')
+    constructor() {
+        super("GameScene");
     }
 
-    this.handleMovement();
-  }
-
-  stopMoving() {
-    if (this.moveEvent) {
-        this.moveEvent.remove(); // Stop the movement loop
-        this.moveEvent = null;
+    preload() {
+        this.load.image("background", "/assets/bg.png");
+        this.load.spritesheet("player", "/assets/bron.png", { frameWidth: 60, frameHeight: 60 });
+        this.load.image("github", "/assets/github-mark.png");
+        this.load.image("tree", "/assets/tree.png");
+        this.load.image("bush", "/assets/bush.png");
+        // paths
+        this.load.image("path-ver", "/assets/path_ver.png");
+        this.load.image("path-hor", "/assets/path_hor.png");
+        this.load.image("path-tr", "/assets/path_tr.png");
+        this.load.image("path-tl", "/assets/path_tl.png");
+        this.load.image("path-bl", "/assets/path_bl.png");
+        this.load.image("path-br", "/assets/path_br.png");
     }
-}
 
+    create() {
+        // background
+        this.bg = this.add.image(window.innerWidth/2, window.innerHeight/2, "background");
+        this.bgWidth = this.bg.width;
+        this.bgHeight = this.bg.height;
+        this.centerX = window.innerWidth/2;
+        this.centerY = window.innerHeight/2;
+        // set cell dimensions
+        this.cellWidth = this.bgWidth / this.dimension;
+        this.cellHeight = this.bgHeight / this.dimension;
+        // trees, paths, bushes
+        this.placeTrees(-20,-20,0,0);
+        this.add.image(window.innerWidth/2 + 250, window.innerHeight/2 + 200, "bush");
+        this.add.image(window.innerWidth/2 + 300, window.innerHeight/2 + 200, "path-ver");
+        this.add.image(window.innerWidth/2 + 350, window.innerHeight/2 + 200, "path-hor");
+        this.add.image(window.innerWidth/2 + 400, window.innerHeight/2 + 200, "path-tr");
+        this.add.image(window.innerWidth/2 + 450, window.innerHeight/2 + 200, "path-tl");
+        this.add.image(window.innerWidth/2 + 500, window.innerHeight/2 + 200, "path-bl");
+        this.add.image(window.innerWidth/2 + 550, window.innerHeight/2 + 200, "path-br");
+        // character
+        this.player = this.physics.add.sprite(window.innerWidth/2, window.innerHeight/2, "player");
+        // character frames
+        this.anims.create({
+            key: "left",
+            frames: [{key: "player", frame: 0}],
+            frameRate: 20
+        });
+        this.anims.create({
+            key: "front",
+            frames: [{key: "player", frame: 1}],
+            frameRate: 20
+        });
+        this.anims.create({
+            key: "right",
+            frames: [{key: "player", frame: 2}],
+            frameRate: 20
+        });
+        // coordinates
+        this.xCoord = this.add.text(20,20,'X: 0', { fontSize: '20px', fill: '#256' });
+        this.xCoord.setScrollFactor(0);
+        this.yCoord = this.add.text(20,40,'Y: 0', { fontSize: '20px', fill: '#256' });
+        this.yCoord.setScrollFactor(0);
+        // links
+        const githubButton = this.add.image(window.innerWidth/2, window.innerHeight/2 - 100, "github").setScale(0.25);
+        githubButton.setInteractive();
+        githubButton.on("pointerdown", () => {
+            window.open("https://github.com/andrewahn-ubc", "_blank"); // Open link in new tab
+        });
+        // welcome
+        this.add.text(window.innerWidth/2 - 400, window.innerHeight/2 - 200, 'Hey, welcome to my website!', { fontSize: '50px', fill: '#000' });
+
+        // bounding player within background
+        this.physics.world.setBounds(
+            -this.bgWidth/2 + window.innerWidth, 
+            -this.bgHeight/2 + window.innerHeight, 
+            this.bgWidth - window.innerWidth, 
+            this.bgHeight - window.innerHeight);
+        this.player.setCollideWorldBounds(true);
+
+        // centering the player in the viewport
+        this.cameras.main.startFollow(this.player, true, 1, 1);
+            
+        if (this.input.keyboard) {
+            this.cursors = this.input.keyboard.createCursorKeys();
+        }
+    }
+
+    update() {
+        this.player.setVelocity(0);
+        // default frame is "front"
+        this.player.anims.play('front')
+        // update coordinates
+        this.xCoord.setText("X: " + this.player.x)
+        this.yCoord.setText("Y: " + this.player.y)
+
+        // handle initial arrow click (without this section, there's a pause before player moves)
+        if (Phaser.Input.Keyboard.JustDown(this.cursors.right)) {
+            this.moveCharacter('right')
+        } else if (Phaser.Input.Keyboard.JustDown(this.cursors.left)) {
+            this.moveCharacter('left')
+        } else if (Phaser.Input.Keyboard.JustDown(this.cursors.up)) {
+            this.moveCharacter('up')
+        } else if (Phaser.Input.Keyboard.JustDown(this.cursors.down)) {
+            this.moveCharacter('down')
+        }
+
+        // handle arrow key "press-and-hold"
+        this.handleMovement();
+
+        // zoom functionality using + and - symbols
+        if (this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.PLUS).isDown) {
+            this.cameras.main.setZoom(Phaser.Math.Clamp(this.cameras.main.zoom + 0.03, 0.1, 2));
+        }
+        if (this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.MINUS).isDown) {
+            this.cameras.main.setZoom(Phaser.Math.Clamp(this.cameras.main.zoom - 0.03, 0.1, 2));
+        }
+    }
+
+    // imagining the background as a dimension x dimension matrix, 
+    // place trees starting at index (startX, startY) and ending at (endX, endY)
+    // input coordinates are in our grid coordinates, not the actual coordinates
+    placeTrees(startX: integer, startY: integer, endX: integer, endY: integer) {
+        if (startX >= endX || startY >= endY || 
+            startX < this.centerX - this.bgWidth/2 || 
+            startY < this.centerY - this.bgHeight/2) {
+            console.log("dimensions are messed up lowkey");
+            return
+        }
+
+        const startX_coord = this.centerX + startX * this.cellWidth
+        const startY_coord = this.centerY + startY * this.cellHeight
+        const endX_coord = this.centerX + endX * this.cellWidth
+        const endY_coord = this.centerY + endY * this.cellHeight
+
+        for (let i = startX_coord; i < endX_coord; i += this.cellWidth) {
+            for (let j = startY_coord; j < endY_coord; j += this.cellHeight) {
+                this.add.image(i, j, "tree");
+            }
+        }
+    }
+
+    stopMoving() {
+        if (this.moveEvent) {
+            this.moveEvent.remove(); // Stop the movement loop
+            this.moveEvent = null;
+        }
+    }
+
+    // helper for deciding how player should move
     handleMovement() {
         if (this.cursors.left.isDown) {
             this.startMoving('left')
@@ -138,13 +186,29 @@ export default class GameScene extends Phaser.Scene {
         }
     }
 
+    // helper for actually moving the player
     moveCharacter(direction: string) {
+    // -this.bgWidth/2 + window.innerWidth, 
+    //     -this.bgHeight/2 + window.innerHeight, 
+    //     this.bgWidth - window.innerWidth, 
+    //     this.bgHeight - window.innerHeight
+        // console.log("player x: ", this.player.x)
+        // console.log('margin: ', -this.bgHeight/2 + window.innerHeight)
+        // console.log('# horizontal steps: ', this.numHor)
+        // if (-this.bgWidth/2 + window.innerWidth >= this.player.x || this.player.x >= this.bgWidth/2 - window.innerWidth) {
+        //     console.log("player out of bounds")
+        //     return
+        // }
         switch (direction) {
             case "left":
+                if (this.player.x - 50 < -this.bgWidth/2 + window.innerWidth) {
+                    console.log("player out of bounds")
+                    return
+                }
                 this.numHor -= 1;
                 this.tweens.add({
                     targets: this.player,  
-                    x: this.initPlayerX + this.numHor * this.cellWidth, 
+                    x: this.centerX + this.numHor * this.cellWidth, 
                     duration: 200,         
                     ease: 'Linear',        
                     repeat: 0,             
@@ -152,10 +216,14 @@ export default class GameScene extends Phaser.Scene {
                 });
                 break;
             case "right":
+                if (this.player.x + 50 >= this.bgWidth/2) {
+                    console.log("player out of bounds")
+                    return
+                }
                 this.numHor += 1
                 this.tweens.add({
                     targets: this.player,  
-                    x: this.initPlayerX + this.numHor * this.cellWidth, 
+                    x: this.centerX + this.numHor * this.cellWidth, 
                     duration: 200,         
                     ease: 'Linear',        
                     repeat: 0,             
@@ -163,10 +231,14 @@ export default class GameScene extends Phaser.Scene {
                 });
                 break;
             case "up":
+                if (this.player.y - 50 < -this.bgHeight/2 + window.innerHeight) {
+                    console.log("player out of bounds")
+                    return
+                }
                 this.numVer -= 1;
                 this.tweens.add({
                     targets: this.player,  
-                    y: this.initPlayerY + this.numVer * this.cellHeight, 
+                    y: this.centerY + this.numVer * this.cellHeight, 
                     duration: 200,         
                     ease: 'Linear',        
                     repeat: 0,             
@@ -174,10 +246,14 @@ export default class GameScene extends Phaser.Scene {
                 });
                 break;
             case "down":
+                if (this.player.y + 50 >= this.bgHeight/2) {
+                    console.log("player out of bounds")
+                    return
+                }
                 this.numVer += 1;
                 this.tweens.add({
                     targets: this.player,  
-                    y: this.initPlayerY + this.numVer * this.cellHeight, 
+                    y: this.centerY + this.numVer * this.cellHeight, 
                     duration: 200,         
                     ease: 'Linear',        
                     repeat: 0,             
